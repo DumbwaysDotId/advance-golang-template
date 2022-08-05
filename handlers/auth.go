4f71cc9ade10d959d5e15757ed5e800a2afadad0
ilham fathullah <ilhamfathullah04@gmail.com>
2022-08-05 15:30:10 +0700

    Hashing password bcrypt

--- handlers/auth.go
+++ handlers/auth.go
@@ -5,11 +5,17 @@ import (
 	dto "dumbmerch/dto/result"
 	"dumbmerch/models"
 	"dumbmerch/pkg/bcrypt"
+	jwtToken "dumbmerch/pkg/jwt"
+	"dumbmerch/pkg/mysql"
 	"dumbmerch/repositories"
 	"encoding/json"
+	"fmt"
+	"log"
 	"net/http"
+	"time"
 
 	"github.com/go-playground/validator/v10"
+	"github.com/golang-jwt/jwt/v4"
 )
 
 type handlerAuth struct {
@@ -63,4 +69,65 @@ func (h *handlerAuth) Register(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	response := dto.SuccessResult{Code: http.StatusOK, Data: convertResponse(data)}
 	json.NewEncoder(w).Encode(response)
+}
+
+func Login(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	request := new(authdto.LoginRequest)
+	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		response := dto.ErrorResult{Code: http.StatusBadRequest, Message: err.Error()}
+		json.NewEncoder(w).Encode(response)
+		return
+	}
+
+	user := models.User{
+		Email:    request.Email,
+		Password: request.Password,
+	}
+
+	// Check email
+	err := mysql.DB.First(&user, "email = ?", user.Email).Error
+
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		response := Result{Code: http.StatusBadRequest, Message: "wrong email or password"}
+		json.NewEncoder(w).Encode(response)
+		return
+	}
+
+	// Check password
+	isValid := password.CheckPasswordHash(newUser.Password, user.Password)
+	if !isValid {
+		w.WriteHeader(http.StatusBadRequest)
+		response := Result{Code: http.StatusBadRequest, Message: "wrong email or password"}
+		json.NewEncoder(w).Encode(response)
+		return
+	}
+
+	//generate token
+	claims := jwt.MapClaims{}
+	claims["id"] = user.ID
+	// claims["email"] = user.Email
+	// claims["status"] = user.Status
+	claims["exp"] = time.Now().Add(time.Hour * 2).Unix() // 2 jam expired
+
+	token, errGenerateToken := jwtToken.GenerateToken(&claims)
+	if errGenerateToken != nil {
+		log.Println(errGenerateToken)
+		fmt.Println("Unauthorize")
+		return
+	}
+
+	result := models.User{
+		ID:    user.ID,
+		Name:  user.Name,
+		Email: user.Email,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	response := Result{Code: http.StatusOK, Message: "success", Data: map[string]interface{}{"user": result, "token": token}}
+	json.NewEncoder(w).Encode(response)
+
 }
