bea85373dd8a814eb1d0eba66432b2eb4f30a225
ilham fathullah <ilhamfathullah04@gmail.com>
2022-08-05 15:18:49 +0700

    Authentication jwt

---
+++ pkg/middleware/auth.go
@@ -0,0 +1,42 @@
+package middleware
+
+import (
+	"context"
+	dto "dumbmerch/dto/result"
+	jwtToken "dumbmerch/pkg/jwt"
+	"encoding/json"
+	"net/http"
+)
+
+type Result struct {
+	Code    int         `json:"code"`
+	Data    interface{} `json:"data"`
+	Message string      `json:"message"`
+}
+
+func Auth(next http.HandlerFunc) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		token := r.Header.Get("token")
+
+		if token == "" {
+			w.WriteHeader(http.StatusUnauthorized)
+			response := dto.ErrorResult{Code: http.StatusBadRequest, Message: "unauthorized"}
+			json.NewEncoder(w).Encode(response)
+			return
+		}
+
+		claims, err := jwtToken.DecodeToken(token)
+
+		if err != nil {
+			w.WriteHeader(http.StatusUnauthorized)
+			response := Result{Code: http.StatusUnauthorized, Message: "unauthorized"}
+			json.NewEncoder(w).Encode(response)
+			return
+		}
+
+		ctx := context.WithValue(r.Context(), "userInfo", claims)
+		r = r.WithContext(ctx)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
