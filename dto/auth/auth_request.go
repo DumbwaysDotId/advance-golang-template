da8db4989b11f60938b06215ec1e150891cd89e5
ilham fathullah <ilhamfathullah04@gmail.com>
2022-08-05 15:27:15 +0700

    Authentication jwt

--- dto/auth/auth_request.go
+++ dto/auth/auth_request.go
@@ -1,7 +1,12 @@
 package authdto
 
 type RegisterRequest struct {
-	Name     string `gorm:"type: varchar(255)" json:"name"`
-	Email    string `gorm:"type: varchar(255)" json:"email"`
-	Password string `gorm:"type: varchar(255)" json:"password"`
+	Name     string `gorm:"type: varchar(255)" json:"name" validate:"required"`
+	Email    string `gorm:"type: varchar(255)" json:"email" validate:"required"`
+	Password string `gorm:"type: varchar(255)" json:"password" validate:"required"`
+}
+
+type LoginRequest struct {
+	Email    string `gorm:"type: varchar(255)" json:"email" validate:"required"`
+	Password string `gorm:"type: varchar(255)" json:"password" validate:"required"`
 }
