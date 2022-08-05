6f37e25061183da4fa285c391fe6e12751825289
ilham fathullah <ilhamfathullah04@gmail.com>
2022-08-05 15:27:28 +0700

    Hashing password bcrypt

--- dto/auth/auth_request.go
+++ dto/auth/auth_request.go
@@ -1,7 +1,7 @@
 package authdto
 
 type RegisterRequest struct {
-	Name     string `gorm:"type: varchar(255)" json:"name"`
-	Email    string `gorm:"type: varchar(255)" json:"email"`
-	Password string `gorm:"type: varchar(255)" json:"password"`
+	Name     string `gorm:"type: varchar(255)" json:"name" validate:"required"`
+	Email    string `gorm:"type: varchar(255)" json:"email" validate:"required"`
+	Password string `gorm:"type: varchar(255)" json:"password" validate:"required"`
 }
