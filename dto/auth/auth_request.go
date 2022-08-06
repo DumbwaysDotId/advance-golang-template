42a8a5e113d26d927aa4ed13fb557050e9fdc589
Jody Septiawan <jody.septiawan5@gmail.com>
2022-08-05 18:56:07 -0700

    Update auth_request.go

--- dto/auth/auth_request.go
+++ dto/auth/auth_request.go
@@ -1,7 +1,6 @@
 package authdto
 
 type AuthRequest struct {
-	ID       int    `json:"id"`
 	Name     string `gorm:"type: varchar(255)" json:"name"`
 	Email    string `gorm:"type: varchar(255)" json:"email"`
 	Password string `gorm:"type: varchar(255)" json:"password"`
